6ee878af4ab12585f0411364f6e91d0114183c95
ricardo-dan-at-212973367909 <ricarfino@gmail.com>
2023-03-15 17:37:57 +0100

    Interface IoT_Platform and clean code

--- App.py
+++ App.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST','GET'])
 def index():
-    sensor_topics = [['/Device/Temperatura-1', 3], ['/Device/Temperatura-2', 3], ['/Device/Humedad-1', 3], ['/Device/Intensidad_Luminosa-1', 3], ]
+    sensor_topics = [['/Device/Temperatura-1', 'Temperatura-1',3], ['/Device/Temperatura-2', 'Temperatura-2',3], ['/Device/Humedad-1', 'Humedad-1',3], ['/Device/Intensidad_Luminosa-1', 'Intensidad_Luminosa-1',3], ]
     output_topics = [['/Device/outputDevice-1', 2], ['/Device/outputDevice-1', 2]]
     if request.method == 'GET':
         return render_template('index.html', sensor_topics=sensor_topics, output_topics = output_topics)
@@ -12,9 +12,9 @@ def index():
         pass
 
 @app.route('/view_data/<name_d>', methods=['POST','GET'])
-def view_topic(name_d):
-    if request.method == 'GET':
-        topic = request.form['device']
+def view_data(name_d):
+    if request.method == 'POST':
+        topic = request.form['topic']
         return render_template('view_data.html', topic=topic, name_d=name_d)
     else:
         pass
